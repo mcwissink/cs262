4ecbea328ff1f23f7a1d37b3102db3b87de87769
Mark Wissink <mwissink.b@gmail.com>
2018-09-26 21:19:52 -0400

    Finished homework01

--- homework01/app/src/main/java/edu/calvin/cs262/mcw33/homework01/MainActivity.java
+++ homework01/app/src/main/java/edu/calvin/cs262/mcw33/homework01/MainActivity.java
@@ -8,12 +8,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private EditText editText1;
     private EditText editText2;
-    private Spinner spinner;
     private String operator;
+    private TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +25,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
         // Get a reference to the value1 and value2
         this.editText1 = findViewById(R.id.value1);
-        this.editText2 = findViewById(R.id.value1);
+        this.editText2 = findViewById(R.id.value2);
 
         // Setup our spinner
-        this.spinner = findViewById(R.id.spinner);
+        Spinner spinner = findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.operators, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        this.resultText = findViewById(R.id.result);
     }
 
     @Override
@@ -50,9 +56,34 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
         // An item was selected. You can retrieve the selected item using
         this.operator = parent.getItemAtPosition(pos).toString();
+        Log.d("TAG", this.operator);
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Do nothing
+    }
+
+    public void calculateResult(View view) {
+        double value1;
+        double value2;
+        try {
+            value1 = Integer.parseInt(this.editText1.getText().toString());
+            value2 = Integer.parseInt(this.editText2.getText().toString());
+        } catch (NumberFormatException e) {
+            this.resultText.setText(R.string.calculate_error);
+            return;
+        }
+
+        double result = 0;
+        switch (this.operator) {
+            case "+": result = value1 + value2; break;
+            case "-": result = value1 - value2; break;
+            case "/": result = value1 / value2; break;
+            case "*": result = value1 * value2; break;
+            default: this.resultText.setText(R.string.calculate_error); break;
+        }
+        DecimalFormat df = new DecimalFormat("#.####");
+        df.setRoundingMode(RoundingMode.CEILING);
+        this.resultText.setText(df.format(result));
     }
 }
